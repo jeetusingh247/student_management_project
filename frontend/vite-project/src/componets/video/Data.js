260db62f0848566e3d84423b1cc6abb7315c6ba1
Saumyasharma974 <sharmasaumya2464@gmail.com>
2024-11-17 10:54:37 +0530

    unit 1 video added

--- frontend/vite-project/src/componets/video/Data.js
+++ frontend/vite-project/src/componets/video/Data.js
@@ -1,10 +1,36 @@
 // data.js
 export const unitVideos = {
-    1: [
-      { id: 'Nnop2walGmM', title: 'Professional Video 1' },
-    { id: 'hxMNYkLN7tI', title: 'Professional Video 2' },
-    { id: 'Qbhx8BEeCm4', title: 'Professional Video 3' },
-    ],
+  1: [
+    { id: 'VOaAuHAwHT4', title: 'Introduction' },
+    { id: 'P1RgK3AAqLQ', title: 'Basic Terminologies - I' },
+    { id: 'QIzpnMtq2ws', title: 'Basic Terminologies - II' },
+    { id: 'wsNPlIRpz40', title: 'Block Diagram of Finite Automata' },
+    { id: 'xZif4Ekykrc', title: 'Introduction to Deterministic FA' },
+    { id: 'u3e25qu_7DQ', title: 'Acceptance of a String by DFA' },
+    { id: 'sbG8ihbQBks', title: 'DFA Designing - I' },
+    { id: 'qXu9nhKLkKc', title: 'DFA Designing - II' },
+    { id: 'LwJXpgl8kKY', title: 'Introduction to Non Deterministic FA' },
+    { id: 'yVF3JxcAzvg', title: 'NDFA Designing' },
+    { id: 'SO0iUc9xizo', title: 'Comparative Study between DFA and NFA' },
+    { id: 'DNDQdd4VLR4', title: 'Divisibility Question of DFA - I' },
+    { id: 'j4c3hSJzkFA', title: 'Divisibility Question of DFA - II' },
+    { id: 'zhnDBaspfGw', title: 'Conversion of NFA to DFA - I' },
+    { id: 'y2LrglBA-J4', title: 'Conversion of NFA to DFA - II' },
+    { id: 'ZCzOfjmp7Bw', title: 'Introduction to Epsilon NFA' },
+    { id: 'FTtJ-e-dOFo', title: 'Conversion of epsilon NFA to NFA - I' },
+    { id: 'lkq0LWi2ACU', title: 'Conversion of epsilon NFA to NFA - II' },
+    { id: 'h4v7x0IMhtI', title: 'Conversion of epsilon NFA to DFA' },
+    { id: 'nJ1McQtigXc', title: 'Minimisation of DFA - I' },
+    { id: 'A7eKj_GXam0', title: 'Minimisation of DFA - II' },
+    { id: 'b7V1hy0TOfY', title: 'Myhill Nerode Theorem of DFA Minimization - I' },
+    { id: 'UiXkJUTkp44', title: 'Myhill Nerode Theorem of DFA Minimization - II' },
+    { id: 'vtKu7gZ8BBA', title: 'FA with output: Introduction to Moore & Mealy Machine' },
+    { id: 'FZgvp0l9o-4', title: 'Construction of Mealy Machine' },
+    { id: 'vmUhGzYUbH0', title: 'Construction of Moore Machine' },
+    { id: 'MsYkgGHR93s', title: 'Conversion of Moore to Mealy Machine' },
+    { id: '_gazATZF0R8', title: 'Conversion of Mealy to Moore Machine' }
+],
+
     2: [
       { id: "sBws8MSXN7A", title: "Video 1: Unit 2 Introduction" },
       { id: "M7lc1UVf-VE", title: "Video 2: Unit 2 Basics" },
